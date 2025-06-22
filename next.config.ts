14cbb35108a6ad7f8eebec0d830f475c4701909e
Dario Skidelsky <76440563+darioque@users.noreply.github.com>
2025-06-21 23:38:35 -0300

    agrega dockerizacion del front

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+ // This includes files from the monorepo base two directories up
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
